ad54968d3716189feeaa6249c5b267aaa59a9f9b
Skylar Bolton <skylar.bolton@gmail.com>
2023-05-07 17:37:56 -0400

    Rate limiter to 15 per hour

--- _middleware/ratelimiter.ts
+++ _middleware/ratelimiter.ts
@@ -2,7 +2,7 @@ import { NextRequest, NextResponse } from "next/server"
 import SlowDown from "slowdown"
 
 const limiter = SlowDown({
-  windowMs: 15 * 60 * 1000, // 15 minutes
+  windowMs: 60 * 60 * 1000, // 15 minutes
   delayAfter: 100, // Allow 100 requests per windowMs without delay
   delayMs: 100, // Add 100ms delay per request above the limit
 })
