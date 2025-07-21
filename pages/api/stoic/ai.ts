72c346cd193206437a608714cdabe62f8a98d5a2
Skylar Bolton <skylar.bolton@gmail.com>
2025-07-21 08:41:44 -0600

    node version 20

--- pages/api/stoic/ai.ts
+++ pages/api/stoic/ai.ts
@@ -1,4 +1,4 @@
-import { NextApiRequest, NextApiResponse } from "next"
+import type { NextApiRequest, NextApiResponse } from "next"
 import { openai } from "../../../lib/openAi"
 
 const stoicResponse = async (question: string) => {
